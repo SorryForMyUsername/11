8998fc6e2a22f1fdce589d3ee1248f4eebb16fe6
Ilya Shichkin <ilyashichkin29@gmail.com>
2024-11-11 20:12:59 +0200

    Задача 2
    Задача 2. Нахождение суммы, разности, произведения и частного двух целых чисел с использованием дополнительных методов.

---
+++ Компьютерный практикум №11/Компьютерный практикум №11/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Компьютерный_практикум__11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x = ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("y = ");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine(
+                $"x + y: {Sum(x, y)}\n" +
+                $"x - y: {Difference(x, y)}\ty - x: {Difference(y, x)}\n" +
+                $"x * y: {Multiply(x, y)}\n" +
+                $"x / y: {Division(x, y)}\ty / x: {Division(y, x)}");
+
+            Console.ReadKey();
+        }
+
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Difference(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int Division(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ Компьютерный практикум №11/Компьютерный практикум №11/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Компьютерный практикум №11")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Компьютерный практикум №11")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("2e6e96cd-3940-4850-afa1-4faa47c4a840")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
