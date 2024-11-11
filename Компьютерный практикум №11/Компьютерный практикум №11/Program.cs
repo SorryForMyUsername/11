d9672741f91f12ea4c25e40277a06306a8564bbb
Ilya Shichkin <ilyashichkin29@gmail.com>
2024-11-11 20:12:59 +0200

    Задача 3
    Задача 3. Вычисление значения функции.

---
+++ Компьютерный практикум №11/Компьютерный практикум №11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Компьютерный_практикум__11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x = ");
+            int x = int.Parse(Console.ReadLine());
+            double y = Function(x);
+            Console.WriteLine($"y = {y}");
+
+            Console.ReadKey();
+        }
+
+        public static double Function(int x)
+        {
+            return x < 5 
+                ? (4 * Math.Pow(x, 2) + 1) / (x - 5)
+                : 3 * Math.Pow(x, 2) - 2;
+        }
+    }
+}
