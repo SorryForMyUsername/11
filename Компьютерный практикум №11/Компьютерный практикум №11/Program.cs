c34274b420d4f8d9a4031cc07220ab38aa6a0b78
Ilya Shichkin <ilyashichkin29@gmail.com>
2024-11-11 20:12:59 +0200

    Задача 1
    Задача 1. Вывод  сообщений приветствия и прощания с использованием дополнительных методов.

---
+++ Компьютерный практикум №11/Компьютерный практикум №11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Компьютерный_практикум__11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello();
+            SayBay();
+
+            Console.ReadKey();
+        }
+
+        public static void SayHello()
+        {
+            Console.WriteLine("Привет!");
+        }
+
+        public static void SayBay()
+        {
+            Console.WriteLine("Пока!");
+        }
+    }
+}
